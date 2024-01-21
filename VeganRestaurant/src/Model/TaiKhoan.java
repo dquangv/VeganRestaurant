9badd0af481c906c1945ed48485f78cea85683c3
VIP <VIP@DESKTOP-ATF752T>
2024-01-21 23:59:15 +0700

    Them NV

---
+++ VeganRestaurant/src/Model/TaiKhoan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Quang
+ */
+public class TaiKhoan {
+    String tenDangNhap;
+    String matKhau;
+    String vaiTro;
+    String nhanVien;
+}
