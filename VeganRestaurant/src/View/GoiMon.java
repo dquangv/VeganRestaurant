e888fefd190c72fb1d3c29321a6404233b7a57fe
weng <buiminhquang2002@gmail.com>
2024-04-04 12:24:01 +0700

    On Update Paying

--- VeganRestaurant/src/View/GoiMon.java
+++ VeganRestaurant/src/View/GoiMon.java
@@ -57,8 +57,11 @@ public class GoiMon extends javax.swing.JPanel {
         thucDonDAO = new ThucDonDAO(new XJdbc());
         loadThucDonToComboBox();
         jScrollPane1.getVerticalScrollBar().setUnitIncrement(20);
+        lblNhanVien.setText(Auth.user.getTenTaiKhoan());
     }
-
+    public void setNhanVien (){
+        lblNhanVien.setText(Auth.user.getTenTaiKhoan());
+    }
     public void setBan(String maBan) {
         lbmaBan.setText("Bàn " + maBan);
     }
@@ -193,6 +196,11 @@ public class GoiMon extends javax.swing.JPanel {
         jButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton2.setForeground(new java.awt.Color(0, 0, 0));
         jButton2.setText("Thanh toán");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         lblNhanVien.setText("Nhân viên:");
 
@@ -365,6 +373,20 @@ public class GoiMon extends javax.swing.JPanel {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        capNhatChiTietGoiMon();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        capNhatChiTietGoiMon();
+        themVaoHoaDon();
+    }//GEN-LAST:event_jButton2ActionPerformed
+    private void hienThiDanhSachMonAn(String loaiMon) {
+        List<MonAn> danhSachMonTheoLoai = thucDonDAO.layDanhSachMonTheoLoai(loaiMon);
+        hienThiDanhSachMonAnUI(danhSachMonTheoLoai);
+    }
+
+    private void capNhatChiTietGoiMon() {
         DefaultTableModel model = (DefaultTableModel) jTable1.getModel();
         ChiTietGoiMonDAO ctgmDAO = new ChiTietGoiMonDAO();
         ctgmDAO.deleteByMaPDB(maPdb);
@@ -385,10 +407,10 @@ public class GoiMon extends javax.swing.JPanel {
         }
 
         JOptionPane.showMessageDialog(this, "Đã lưu thông tin vào bảng ChiTietGM.");
-    }//GEN-LAST:event_jButton1ActionPerformed
-    private void hienThiDanhSachMonAn(String loaiMon) {
-        List<MonAn> danhSachMonTheoLoai = thucDonDAO.layDanhSachMonTheoLoai(loaiMon);
-        hienThiDanhSachMonAnUI(danhSachMonTheoLoai);
+    }
+
+    private void themVaoHoaDon() {
+
     }
 
     private void hienThiDanhSachMonAnUI(List<MonAn> danhSachMonAn) {
@@ -507,7 +529,7 @@ public class GoiMon extends javax.swing.JPanel {
                 stt++,
                 row.getTenMonAn(),
                 row.getSoLuong() + "",
-                decimalFormat.format(row.getDonGia()), 
+                decimalFormat.format(row.getDonGia()),
                 decimalFormat.format(row.getThanhTien()),
                 row.getGhiChu()
             };
